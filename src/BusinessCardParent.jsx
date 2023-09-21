ec5ce009d193381ec6e146ac5c2c2fcbb734aa6b
Alex H <alex.holder@bigfootds.com>
2023-09-21 20:23:35 +1000

    feat: state in parent, passed to props of display

--- src/BusinessCardParent.jsx
+++ src/BusinessCardParent.jsx
@@ -5,14 +5,30 @@ import BusinessCardDisplay from "./BusinessCardDisplay";
 export default class BusinessCardParent extends Component {
 	constructor(){
 		super();
+
+		this.state = {
+			editMode: false,
+			name: "Example name",
+			email:"example@email.com"
+		}
+
+
 	}
 
 	render(){
-		return(
-			<div>
-				<BusinessCardDisplay></BusinessCardDisplay>
-			</div>
-		)
+		if (this.state.editMode){
+			// if edit is true, show the form 
+		} else {
+			return(
+				<div>
+					<BusinessCardDisplay
+					email={this.state.email}
+					name={this.state.name}
+					 
+					 />
+				</div>
+			)
+		}
 	}
 }
 
