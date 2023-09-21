61955c9dd82406dc08dce9bd97a1f13e1c0f3cee
Alex H <alex.holder@bigfootds.com>
2023-09-21 21:00:53 +1000

    fix: removing state from form

--- src/BusinessCardForm.jsx
+++ src/BusinessCardForm.jsx
@@ -2,15 +2,6 @@ import { Component } from "react";
 
 
 export default class BusinessCardForm extends Component {
-	constructor(props){
-		super(props);
-
-		this.state = {
-			name: this.props.name,
-			email: this.props.email
-		}
-	}
-
 	handleChangeInput = (event) => {
 		// pass in the form input data 
 		// call the parent setState function 
