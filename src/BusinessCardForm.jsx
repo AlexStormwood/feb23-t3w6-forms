5fff681d6c4f964b4162134d28c434a41372d43e
Alex H <alex.holder@bigfootds.com>
2023-09-21 20:58:53 +1000

    fix: form input updates properly

--- src/BusinessCardForm.jsx
+++ src/BusinessCardForm.jsx
@@ -11,12 +11,22 @@ export default class BusinessCardForm extends Component {
 		}
 	}
 
+	handleChangeInput = (event) => {
+		// pass in the form input data 
+		// call the parent setState function 
+
+		this.props.setParentState(event.target.name, event.target.value);
+	}
+
 	render(){
 		return(
 			<form>
 				{/* htmlFor instead of for for the JSX compiler */}
-				<label htmlFor="nameInput" >Name:</label>
-				<input type="text" name="nameInput" id="nameInput" value={this.state.name} />
+				<label htmlFor="name" >Name:</label>
+				<input type="text" name="name" id="nameInput" 
+				value={this.props.name} 
+				onChange={this.handleChangeInput}
+				/>
 			</form>
 		)
 	}
