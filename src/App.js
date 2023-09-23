68821f7c538289fc84c124925729cfafa32960c5
Alex H <alex.holder@bigfootds.com>
2023-09-23 10:44:20 +1000

    feat: 2 routes set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+
+// React Router parts:
+// - BrowserRouter needs to exist higher up in the component tree than whatever we are writing Routes in
+// - Routes need to exist, determine what content is rendered on which route eg. /about for <About /> 
+// - Route specifies URL and component, belongs to Routes
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import BusinessCardParent from "./BusinessCardParent";
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
     <div className="App">
-        <BusinessCardParent />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
 
-        <BusinessCardParent />
+        <Route path="/about" element={<AboutPage />} />
+
+      </Routes>
 
     </div>
   );
