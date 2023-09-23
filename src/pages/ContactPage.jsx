b3d1d7c5d1122b97cf159a66b7c20a5ca411a81a
Alex H <alex.holder@bigfootds.com>
2023-09-23 12:01:58 +1000

    fix: contact page uses redirect component

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,10 +1,11 @@
+import RedirectHomeButton from "../components/RedirectHomeButton";
 
 export default function ContactPage(props){
 	return(
 		<div>
 			<h1>Contact Us</h1>
 
-			
+			<RedirectHomeButton />
 		</div>
 	)
 }
